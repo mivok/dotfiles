e22d71d1668596ffb946a0e97982a2eba74117fa
Mark Harrison <mark@mivok.net>
2021-06-03 00:33:31 -0400

    Initial wezterm config

---
+++ terminal/.config/wezterm/wezterm.lua
@@ -0,0 +1,62 @@
+local wezterm = require 'wezterm';
+
+-- Create a new tab, split into 4 equal sized panes, 2x2
+wezterm.on("new-4up-tab", function(window, pane)
+  window:perform_action(wezterm.action{SpawnTab="CurrentPaneDomain"}, pane)
+  window:perform_action(wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}, pane)
+  window:perform_action(wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}, pane)
+  window:perform_action(wezterm.action {ActivatePaneDirection = "Left"}, pane)
+  window:perform_action(wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}, pane)
+  window:perform_action(wezterm.action {ActivatePaneDirection = "Up"}, pane)
+end)
+
+return {
+  font = wezterm.font_with_fallback({
+    "Hack Nerd Font", -- The main font I want to use
+    "Menlo", -- This has the check mark and cross symbols
+    "Apple Color Emoji" -- I prefer apple emoji to google emoji
+  }),
+  font_size = 14,
+  color_scheme = "Builtin Tango Dark",
+  window_background_opacity = 0.8,
+  initial_cols = 161,
+  initial_rows = 49,
+  pane_focus_follows_mouse = true,
+  exit_behavior = "Close",
+  -- Enable once https://github.com/wez/wezterm/issues/797 is in a release
+  --tab_bar_at_bottom = true,
+  -- I like CTRL-A as a terminal prefix, like screen
+  leader = {key = "a", mods = "CTRL", timeout_milliseconds = 1000},
+  keys = {
+    -- Pane splitting, like I had set up with tmux
+    {
+      key = "|",
+      mods = "LEADER|SHIFT",
+      action = wezterm.action {SplitHorizontal = {domain = "CurrentPaneDomain"}}
+    }, {
+      key = "-",
+      mods = "LEADER",
+      action = wezterm.action {SplitVertical = {domain = "CurrentPaneDomain"}}
+    }, {
+      key = "LeftArrow",
+      mods = "LEADER",
+      action = wezterm.action {ActivatePaneDirection = "Left"}
+    }, {
+      key = "RightArrow",
+      mods = "LEADER",
+      action = wezterm.action {ActivatePaneDirection = "Right"}
+    }, {
+      key = "UpArrow",
+      mods = "LEADER",
+      action = wezterm.action {ActivatePaneDirection = "Up"}
+    }, {
+      key = "DownArrow",
+      mods = "LEADER",
+      action = wezterm.action {ActivatePaneDirection = "Down"}
+    }, {
+      key = "t",
+      mods = "LEADER",
+      action = wezterm.action {EmitEvent="new-4up-tab"}
+    }
+  }
+}
