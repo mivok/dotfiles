4739fc1b703f1f0a2f1c982f041e23370f3248e9
Mark Harrison <mark@mivok.net>
2021-06-16 10:48:54 -0400

    Revert to using vim-terraform for terraform fmt on save

--- nvim/.config/nvim/lua/lsp/terraform-ls.lua
+++ nvim/.config/nvim/lua/lsp/terraform-ls.lua
@@ -1,7 +1,3 @@
 -- brew install hashicorp/tap/terraform-ls
 -- or download binary from https://releases.hashicorp.com/terraform-ls/
 require('lspconfig').terraformls.setup {}
-
--- Autoformat on save
-vim.cmd(
-    [[au BufWritePre *.tf,*.tfvars lua vim.lsp.buf.formatting_sync(nil, 100)]])
