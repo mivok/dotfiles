4fbb3724f47c7937b5c6e9519f19f22aee195615
Mark Harrison <mark@mivok.net>
2022-05-05 12:32:45 -0400

    Disable highlighting on paste

--- zsh/.zsh/conf.d/appsettings.zsh
+++ zsh/.zsh/conf.d/appsettings.zsh
@@ -40,6 +40,12 @@ autoload -Uz bracketed-paste-magic
 zle -N bracketed-paste bracketed-paste-magic
 zstyle ':bracketed-paste-magic' active-widgets '.self-*'
 
+# Disable highlighting on paste (which confuses the heck out of me because
+# it looks like the text is selected and that it's going to overwrite what I
+# pasted)
+typeset -a zle_highlight
+zle_highlight+=(paste:none)
+
 # History behavior
 setopt SHARE_HISTORY
 setopt HIST_IGNORE_SPACE
