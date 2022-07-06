7265ff769dfb720c676fdebdb106a13318d5da2c
Mark Harrison <mark@mivok.net>
2022-07-06 10:42:11 -0400

    Fix homebrew loading

--- zsh/.zsh/conf.d/pre.zsh
+++ zsh/.zsh/conf.d/pre.zsh
@@ -5,3 +5,8 @@
 if [[ -e /opt/homebrew/opt/fzf ]]; then
     export FZF_BASE=/opt/homebrew/opt/fzf
 fi
+
+# Add homebrew to path
+if [[ -f /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
