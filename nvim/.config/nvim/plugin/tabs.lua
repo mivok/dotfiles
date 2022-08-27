93295fa96e804c58e0f5fc6659ccf9d2c6903f9d
Mark Harrison <mark@mivok.net>
2022-08-27 00:30:00 -0400

    Move some autocommands to lua

--- nvim/.config/nvim/plugin/tabs.lua
+++ nvim/.config/nvim/plugin/tabs.lua
@@ -1,8 +1,9 @@
 -- Helper function to set the tab size and hard vs soft tabs
-function set_tab_width(width, hard_tab)
+function set_tab_width(width, opts)
+	opts = opts or {}
   vim.opt_local.shiftwidth = width
   vim.opt_local.softtabstop = width
-  if hard_tab then
+  if opts.hard_tab then
     vim.opt_local.tabstop = width
     vim.opt_local.expandtab = false
     -- Don't show hard tab chars if we are using
@@ -16,8 +17,8 @@ end
 
 -- Create :TabN and :RealTabN commands
 local newcmd = vim.api.nvim_create_user_command
-newcmd('Tab2', function() set_tab_width(2, false) end, {})
-newcmd('Tab4', function() set_tab_width(4, false) end, {})
-newcmd('RealTab2', function() set_tab_width(2, true) end, {})
-newcmd('RealTab4', function() set_tab_width(4, true) end, {})
-newcmd('RealTab8', function() set_tab_width(8, true) end, {})
+newcmd('Tab2', function() set_tab_width(2) end, {})
+newcmd('Tab4', function() set_tab_width(4) end, {})
+newcmd('RealTab2', function() set_tab_width(2, {hard_tab = true}) end, {})
+newcmd('RealTab4', function() set_tab_width(4, {hard_tab = true}) end, {})
+newcmd('RealTab8', function() set_tab_width(8, {hard_tab = true}) end, {})
