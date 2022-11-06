97bbe97d1228c3172202b68468969444436844cd
Mark Harrison <mark@mivok.net>
2022-11-06 00:53:13 -0400

    Fix nvim deprecation

--- nvim/.config/nvim/lua/plugin-nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugin-nvim-cmp.lua
@@ -74,8 +74,7 @@ cmp.setup {
 
 -- Update capabilities of the client so that LSP servers may return better
 -- completion options
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require('lspconfig')
 if not lspconfig then
