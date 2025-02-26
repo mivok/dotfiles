c3cce3bec27e332ab276ad96858c5bef157a225e
Mark Harrison <mark@mivok.net>
2025-02-26 00:12:33 -0500

    Remove some old aliases

--- zsh/.zsh/conf.d/aliases.zsh
+++ zsh/.zsh/conf.d/aliases.zsh
@@ -9,30 +9,10 @@ alias mdig='dig @224.0.0.251 -p 5353 +time=1 +tries=1'
 # Perl timestamp alias
 alias perlts='perl -p -e "s/^(\d+)/localtime(\$1)/e"'
 
-# List running vms
-alias runningvms="VBoxManage list runningvms | awk -F'\"' '{print \$2}'"
-alias runningcontainers="docker ps --format '{{.Image}}\\t{{.Names}}'"
-
-# Saml2aws login alias
-alias s2al='saml2aws login --skip-prompt'
-
-# Azure logins
-alias azl='az login --tenant valimailappprod.onmicrosoft.com'
-alias azs='az account set --subscription "Valimail Staging"'
-alias azp='az account set --subscription "Valimail Prod"'
-
 # Use nvim if available
 if (($+commands[nvim])); then
     alias vim=nvim
 fi
 
-# Use hub command if available
-if (($+commands[hub])); then
-    alias git=hub
-fi
-
 # Ensure minikube uses its own config file
 alias minikube='KUBECONFIG=$HOME/.kube/minikube.config minikube'
-
-# Quickly make a new python venv using direnv
-alias py3venv='echo layout python3 > .envrc; direnv allow'
