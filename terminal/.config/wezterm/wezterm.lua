365346aa3ef82f78f509e48f829fb6b943748625
Mark Harrison <mark@mharrison.org>
2025-04-17 18:02:02 -0400

    Update config to use newer conventions

--- terminal/.config/wezterm/wezterm.lua
+++ terminal/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm';
+local config = wezterm.config_builder()
 
 -- Resize the window (called by event handlers)
 function resize_window(window, pane, cols, rows)
@@ -13,7 +14,7 @@ end
 -- This is mostly useful for getting the pixel_width and pixel_height for a
 -- pane.
 function pane_info(pane)
-  for _, p in pairs(pane:mux_pane():tab():panes_with_info()) do
+  for _, p in pairs(pane:tab():panes_with_info()) do
     if p.pane:pane_id() == pane:pane_id() then
       return p
     end
@@ -36,13 +37,7 @@ end)
 wezterm.on("new-pane-auto", function(window, _)
   -- Get the MuxTab object for the active tab, so we can go through the
   -- panes one by one
-  local mux_window = window:mux_window()
-  local active_tab
-  for _, t in pairs(mux_window:tabs_with_info()) do
-    if t.is_active then
-      active_tab = t.tab
-    end
-  end
+  local active_tab = window:active_tab()
 
   -- Identify the largest pane (and also which direction to split it in)
   local largest_pane
@@ -77,130 +72,135 @@ wezterm.on("new-pane-auto", function(window, _)
   largest_pane:split{direction=direction}
 end)
 
-return {
-  -- I don't want the update pop ups
-  check_for_updates = false,
+-- I don't want the update pop ups
+config.check_for_updates = false
 
-  -- Font config
-  font = wezterm.font_with_fallback({
-    "Hack", -- The main font I want to use
-    "Apple Color Emoji" -- I prefer apple emoji if they're present
-  }),
-  font_size = 12,
-  cell_width = 1.1,
+-- Font config
+config.font = wezterm.font_with_fallback({
+  "Hack", -- The main font I want to use
+  "Apple Color Emoji" -- I prefer apple emoji if they're present
+})
+config.font_size = 12
+config.cell_width = 1.1
 
-  color_scheme = "Builtin Tango Dark",
-  window_background_opacity = 0.8,
-  initial_cols = 173,
-  initial_rows = 49,
+config.color_scheme = "Builtin Tango Dark"
+config.window_background_opacity = 0.8
+config.initial_cols = 173
+config.initial_rows = 49
 
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0
-  },
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0
+}
 
-  -- Don't dim inactive panes
-  inactive_pane_hsb = {
-    saturation = 1.0,
-    brightness = 1.0,
-  },
-  exit_behavior = "Close",
+-- Don't dim inactive panes
+config.inactive_pane_hsb = {
+  saturation = 1.0,
+  brightness = 1.0,
+}
 
-  -- Tab bar behavior
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+-- Always close the pane on process exit
+config.exit_behavior = "Close"
 
-  -- Set CTRL-A as the LEADER modification
-  leader = {key = "a", mods = "CTRL", timeout_milliseconds = 1000},
-  keys = {
-    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-    {
-      key="a",
-      mods="LEADER|CTRL",
-      action=wezterm.action.SendString("\x01")
-    }, {
-      key = "|",
-      mods = "LEADER|SHIFT",
-      action = wezterm.action.SplitHorizontal
-    }, {
-      key = "-",
-      mods = "LEADER",
-      action = wezterm.action.SplitVertical
-    }, {
-      key = "Enter",
-      mods = "LEADER",
-      action = wezterm.action.EmitEvent("new-pane-auto")
-    }, {
-      key = "=",
-      mods = "LEADER",
-      action = wezterm.action.EmitEvent("split-auto")
-    }, {
-      key = "LeftArrow",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Left")
-    }, {
-      key = "RightArrow",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Right")
-    }, {
-      key = "UpArrow",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Up")
-    }, {
-      key = "DownArrow",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Down")
-    }, {
-      key = "0",
-      mods = "LEADER",
-      action = "ResetFontAndWindowSize"
-    }, {
-      key = "1",
-      mods = "LEADER",
-      action = wezterm.action_callback(function(window, pane)
-        resize_window(window, pane, 173, 49)
-      end)
-    }, {
-      key = "2",
-      mods = "LEADER",
-      action = wezterm.action_callback(function(window, pane)
-        resize_window(window, pane, 201, 49)
-      end)
-    }, {
-      key = "3",
-      mods = "LEADER",
-      action = wezterm.action_callback(function(window, pane)
-        resize_window(window, pane, 110, 35)
-      end)
-    }, {
-      key = "?",
-      mods = "LEADER|SHIFT",
-      action = wezterm.action.SpawnCommandInNewTab {
-        args={"sh", "-c", "wezterm show-keys | less"}
+-- Tab bar behavior
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Set CTRL-A as the LEADER modification
+config.leader = {key = "a", mods = "CTRL", timeout_milliseconds = 1000}
+config.keys = {
+  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+  {
+    key="a",
+    mods="LEADER|CTRL",
+    action=wezterm.action.SendString("\x01")
+  }, {
+    key = "|",
+    mods = "LEADER|SHIFT",
+    action = wezterm.action.SplitHorizontal
+  }, {
+    key = "-",
+    mods = "LEADER",
+    action = wezterm.action.SplitVertical
+  }, {
+    key = "Enter",
+    mods = "LEADER",
+    action = wezterm.action.EmitEvent("new-pane-auto")
+  }, {
+    key = "=",
+    mods = "LEADER",
+    action = wezterm.action.EmitEvent("split-auto")
+  }, {
+    key = "LeftArrow",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Left")
+  }, {
+    key = "RightArrow",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Right")
+  }, {
+    key = "UpArrow",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Up")
+  }, {
+    key = "DownArrow",
+    mods = "LEADER",
+    action = wezterm.action.ActivatePaneDirection("Down")
+  }, {
+    key = "0",
+    mods = "LEADER",
+    action = "ResetFontAndWindowSize"
+  }, {
+    key = "1",
+    mods = "LEADER",
+    action = wezterm.action_callback(function(window, pane)
+      resize_window(window, pane, 173, 49)
+    end)
+  }, {
+    key = "2",
+    mods = "LEADER",
+    action = wezterm.action_callback(function(window, pane)
+      resize_window(window, pane, 201, 49)
+    end)
+  }, {
+    key = "3",
+    mods = "LEADER",
+    action = wezterm.action_callback(function(window, pane)
+      resize_window(window, pane, 110, 35)
+    end)
+  }, {
+    key = "?",
+    mods = "LEADER|SHIFT",
+    action = wezterm.action.SpawnCommandInNewTab {
+      args={"sh", "-c",
+        wezterm.procinfo.executable_path_for_pid(wezterm.procinfo.pid()) ..
+        " show-keys | less"
       }
     }
-  },
-  mouse_bindings = {
-    -- Allow Cmd or CTRL click to open links like in iterm2
-    {
-      event={Up={streak=1, button="Left"}},
-      mods="SUPER",
-      action="OpenLinkAtMouseCursor",
-    },
-    {
-      event={Up={streak=1, button="Left"}},
-      mods="CTRL",
-      action="OpenLinkAtMouseCursor",
-    },
-    -- Change the default click behavior so that it only selects
-    -- text and doesn't open hyperlinks
-    {
-      event={Up={streak=1, button="Left"}},
-      mods="NONE",
-      action=wezterm.action{CompleteSelection="PrimarySelection"},
-    },
   }
 }
+config.mouse_bindings = {
+  -- Allow Cmd or CTRL click to open links like in iterm2
+  {
+    event={Up={streak=1, button="Left"}},
+    mods="SUPER",
+    action="OpenLinkAtMouseCursor",
+  },
+  {
+    event={Up={streak=1, button="Left"}},
+    mods="CTRL",
+    action="OpenLinkAtMouseCursor",
+  },
+  -- Change the default click behavior so that it only selects
+  -- text and doesn't open hyperlinks
+  {
+    event={Up={streak=1, button="Left"}},
+    mods="NONE",
+    action=wezterm.action{CompleteSelection="PrimarySelection"},
+  },
+}
+
+return config
