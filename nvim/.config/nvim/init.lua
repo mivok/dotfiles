df4582c5c2d1f76054d6a042115d1086d7dc1923
Mark Harrison <mark@mharrison.org>
2025-04-27 18:36:41 -0400

    Move plugin/*.lua to lua/config/*.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,14 @@
+-- Basic vim settings
+require('config.settings')
+require('config.autocommands')
+require('config.mappings')
+require('config.python_venv')
+
+-- Load lazy.nvim and plugins
 require('config.lazy')
+
+-- Additional config
+require('config.lsp')
+-- Custom commands
+require('config.softwrap')
+require('config.tabs')
