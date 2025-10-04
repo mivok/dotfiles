30fa01ab5d4625e85e5887220bd3f0255a141e15
Mark Harrison <mark@mharrison.org>
2025-10-04 15:19:33 -0400

    Add an adopt script

---
+++ adopt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Moves a pre-existing file (or directory) into the dotfiles repo, adopting it
+# with stow.
+#
+# Usage: ./adopt.sh FILE STOW_DIR
+# Example: ./adopt.sh ~/.config/foo/foo.conf foo
+# This will move ~/.config/foo/foo.conf to ./foo/.config/foo/foo.conf and run
+# stow to create the symlink. Directories will be created as needed.
+
+FILE="$1"
+STOW_DIR="$2"
+cd "$(dirname "$0")" || exit 1
+
+if [[ -z "$FILE" || -z "$STOW_DIR" ]]; then
+    echo "Usage: $0 FILE STOW_DIR"
+    exit 1
+fi
+
+REL_PATH="${FILE/#$HOME\//}"
+REL_DIR="${REL_PATH%/*}"
+
+if [[ "$REL_PATH" == "$FILE" ]]; then
+    echo "Error: FILE must be inside the home directory"
+    exit 1
+fi
+
+mkdir -p "$STOW_DIR/$REL_DIR"
+mv "$FILE" "$STOW_DIR/$REL_PATH"
+stow -v "$STOW_DIR"
