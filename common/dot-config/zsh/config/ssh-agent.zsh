39368a160d71a4b2c7a2cf482054dbe7b39920fc
Mark Harrison <mark@mharrison.org>
2026-09-21 23:47:53 -0400

    Make ssh agent work with gui apps on linux/kde

--- common/dot-config/zsh/config/ssh-agent.zsh
+++ common/dot-config/zsh/config/ssh-agent.zsh
@@ -4,6 +4,14 @@ setup_ssh_agent() {
     local SSH_ENV_FILE="$SSH_ENV_DIR/ssh-agent.env"
     mkdir -p "$SSH_ENV_DIR"
 
+    # On linux, use the systemd service if available
+    # This requires a one-time setup command:
+    # systemctl --user enable --now ssh-agent.socket
+    if [[ -z "$SSH_AUTH_SOCK" && "$OSTYPE" == linux* && \
+          -n "$XDG_RUNTIME_DIR" && -S "$XDG_RUNTIME_DIR/ssh-agent.socket" ]]; then
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+    fi
+
     if [[ -z "$SSH_AUTH_SOCK" ]]; then
         if [[ -f "$SSH_ENV_FILE" ]]; then
             source "$SSH_ENV_FILE" > /dev/null
